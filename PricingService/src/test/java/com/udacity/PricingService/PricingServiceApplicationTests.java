ee8a5ae1ac01bc9017001f511a137750c6fc429b
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-02-08 23:20:05 -0500

    Vehicle API/Tests

--- PricingService/src/test/java/com/udacity/PricingService/PricingServiceApplicationTests.java
+++ PricingService/src/test/java/com/udacity/PricingService/PricingServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.udacity.PricingService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class PricingServiceApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
